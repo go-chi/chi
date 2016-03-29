3918c16acccb28ce81646910efadf50bde8041b3
Peter Kieltyka <peter.kieltyka@pressly.com>
2016-03-29 16:53:34 -0400

    Drain response body in render Bind

--- render/bind.go
+++ render/bind.go
@@ -3,8 +3,14 @@ package render
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
 )
 
 func Bind(r io.Reader, v interface{}) error {
-	return json.NewDecoder(r).Decode(v)
+	err := json.NewDecoder(r).Decode(v)
+	io.Copy(ioutil.Discard, r)
+	if err != nil {
+		return err
+	}
+	return nil
 }
