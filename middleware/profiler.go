17d81d1e3212898532a03d6332233e4b30c914ab
Peter Kieltyka <peter.kieltyka@pressly.com>
2016-04-02 08:45:47 -0400

    Profiler middleware

---
+++ middleware/profiler.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"expvar"
+	"fmt"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/pressly/chi"
+	"golang.org/x/net/context"
+)
+
+func Profiler() http.Handler {
+	r := chi.NewRouter()
+	r.Use(NoCache)
+
+	r.Get("/", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, r.RequestURI+"/pprof/", 301)
+	})
+	r.Handle("/pprof", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, r.RequestURI+"/", 301)
+	})
+	r.Handle("/pprof/", pprof.Index)
+	r.Handle("/pprof/cmdline", pprof.Cmdline)
+	r.Handle("/pprof/profile", pprof.Profile)
+	r.Handle("/pprof/symbol", pprof.Symbol)
+	r.Handle("/pprof/block", pprof.Handler("block").ServeHTTP)
+	r.Handle("/pprof/heap", pprof.Handler("heap").ServeHTTP)
+	r.Handle("/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
+	r.Handle("/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+	r.Handle("/vars", expVars)
+
+	return r
+}
+
+// Replicated from expvar.go as not public.
+func expVars(w http.ResponseWriter, r *http.Request) {
+	first := true
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, "{\n")
+	expvar.Do(func(kv expvar.KeyValue) {
+		if !first {
+			fmt.Fprintf(w, ",\n")
+		}
+		first = false
+		fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
+	})
+	fmt.Fprintf(w, "\n}\n")
+}
