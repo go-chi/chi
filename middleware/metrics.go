4a03dc8e0fb2b90ded826b0b614a817b8050d6df
Thomas LE ROUX <thomas@november-eleven.fr>
2016-06-28 00:28:26 +0200

    refactor(metrics): Initial release of metrics from Logger engine

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -5,91 +5,81 @@ package middleware
 
 import (
 	"bufio"
-	"bytes"
+	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"time"
 )
 
-// Logger is a middleware that logs the start and end of each request, along
-// with some useful data about what was requested, what the response status was,
-// and how long it took to return. When standard output is a TTY, Logger will
-// print in color, otherwise it will print in black and white.
-//
-// Logger prints a request ID if one is provided.
-func Logger(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		reqID := GetReqID(r.Context())
-		prefix := requestPrefix(reqID, r)
-		lw := wrapWriter(w)
-
-		t1 := time.Now()
-		defer func() {
-			t2 := time.Now()
-			printRequest(prefix, reqID, lw, t2.Sub(t1))
-		}()
-
-		next.ServeHTTP(lw, r)
-	}
-
-	return http.HandlerFunc(fn)
+// Metric is a composition of various statistics from a request, such as its
+// execution time and the response status.
+type Metric struct {
+	RequestID     string
+	Method        string
+	URL           string
+	RemoteAddr    string
+	Status        int
+	BytesWritten  int
+	ExecutionTime time.Duration
 }
 
-func requestPrefix(reqID string, r *http.Request) *bytes.Buffer {
-	buf := &bytes.Buffer{}
-
-	if reqID != "" {
-		cW(buf, nYellow, "[%s] ", reqID)
-	}
-	cW(buf, nCyan, "\"")
-	cW(buf, bMagenta, "%s ", r.Method)
-
-	if r.TLS == nil {
-		cW(buf, nCyan, "http://%s%s %s\" ", r.Host, r.RequestURI, r.Proto)
-	} else {
-		cW(buf, nCyan, "https://%s%s %s\" ", r.Host, r.RequestURI, r.Proto)
-	}
-
-	buf.WriteString("from ")
-	buf.WriteString(r.RemoteAddr)
-	buf.WriteString(" - ")
-
-	return buf
+// MetricsHandler is a Metric receiver.
+type MetricsHandler interface {
+	Add(metric Metric)
 }
 
-func printRequest(buf *bytes.Buffer, reqID string, w writerProxy, dt time.Duration) {
-	status := w.Status()
-	if status == StatusClientClosedRequest {
-		cW(buf, bRed, "[disconnected]")
-	} else {
-		switch {
-		case status < 200:
-			cW(buf, bBlue, "%03d", status)
-		case status < 300:
-			cW(buf, bGreen, "%03d", status)
-		case status < 400:
-			cW(buf, bCyan, "%03d", status)
-		case status < 500:
-			cW(buf, bYellow, "%03d", status)
-		default:
-			cW(buf, bRed, "%03d", status)
+// Metrics return a middleware which will use the given MetricsHandler in
+// order to publish new statistics.
+func Metrics(handler MetricsHandler) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+
+			reqID := GetReqID(r.Context())
+			url := getRequestURL(r)
+
+			lw := wrapWriter(w)
+
+			t1 := time.Now()
+			defer func() {
+				t2 := time.Now()
+
+				handler.Add(Metric{
+					RequestID:     reqID,
+					Method:        r.Method,
+					URL:           url,
+					RemoteAddr:    r.RemoteAddr,
+					Status:        lw.Status(),
+					BytesWritten:  lw.BytesWritten(),
+					ExecutionTime: t2.Sub(t1),
+				})
+
+			}()
+
+			next.ServeHTTP(lw, r)
 		}
+
+		return http.HandlerFunc(fn)
+	}
+}
+
+var xForwardedProto = http.CanonicalHeaderKey("X-Forwarded-Proto")
+
+func getRequestURL(r *http.Request) string {
+
+	scheme := "http"
+
+	// Is HTTPS with TLS connection ?
+	if r.TLS != nil {
+		scheme = "https"
 	}
 
-	cW(buf, bBlue, " %dB", w.BytesWritten())
-
-	buf.WriteString(" in ")
-	if dt < 500*time.Millisecond {
-		cW(buf, nGreen, "%s", dt)
-	} else if dt < 5*time.Second {
-		cW(buf, nYellow, "%s", dt)
-	} else {
-		cW(buf, nRed, "%s", dt)
+	// or with a reverse proxy ?
+	if xfp := r.Header.Get(xForwardedProto); xfp == "https" {
+		scheme = "https"
 	}
 
-	log.Print(buf.String())
+	return fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
 }
 
 // writerProxy is a proxy around an http.ResponseWriter that allows you to hook
