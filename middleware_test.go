6a63b7e1c35255a7f7f4edd2e03ae991b6f8ea91
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2016-08-04 11:25:13 -0400

    Add more tests for middleware stack

--- middleware_test.go
+++ middleware_test.go
@@ -35,16 +35,21 @@ func TestMiddlewareStack(t *testing.T) {
 			r.Use(mwIncreaseCounter, mwIncreaseCounter) // counter == 3
 			r.Get("/3", handlerPrintCounter)
 		})
-		r.Route("/4", func(r Router) {
+		r.Route("/", func(r Router) {
 			r.Use(mwIncreaseCounter, mwIncreaseCounter) // counter == 3
-			r.Get("/", Use(mwIncreaseCounter).HandlerFunc(handlerPrintCounter))
+			r.Get("/4", Use(mwIncreaseCounter).HandlerFunc(handlerPrintCounter))
+			r.Group(func(r Router) {
+				r.Use(mwIncreaseCounter, mwIncreaseCounter) // counter == 5
+				r.Get("/5", handlerPrintCounter)
+				r.Get("/6", Use(mwIncreaseCounter).HandlerFunc(handlerPrintCounter))
+			})
 		})
 	})
 
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	for _, route := range []string{"0", "1", "2", "3", "4"} {
+	for _, route := range []string{"0", "1", "2", "3", "4", "5", "6"} {
 		t.Run(route, func(t *testing.T) {
 			if _, body := testRequest(t, ts, "GET", "/"+route, nil); body != route {
 				t.Errorf("expected %v, got %v", route, body)
