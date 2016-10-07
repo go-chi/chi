f66859c490bbbb1234a268d91cb0783e241ed52b
Peter Kieltyka <peter.kieltyka@pressly.com>
2016-10-07 11:10:00 -0400

    Graceful shutdown example using goware/valve and tylerb/graceful

---
+++ _examples/graceful/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/goware/valve"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+	"github.com/tylerb/graceful"
+)
+
+func main() {
+
+	// Our graceful valve shut-off package to manage code preemption and
+	// shutdown signaling.
+	valv := valve.New()
+	baseCtx := valv.ContextLever()
+
+	// Example of a long running background worker thing..
+	go func(ctx context.Context) {
+		for {
+			<-time.After(1 * time.Second)
+
+			func() {
+				valve.Context(ctx).Open()
+				defer valve.Context(ctx).Close()
+
+				// actual code doing stuff..
+				fmt.Println("tick..")
+				time.Sleep(2 * time.Second)
+				// end-logic
+
+				// signal control..
+				select {
+				case <-valve.Context(ctx).Stop():
+					fmt.Println("valve is closed")
+					return
+
+				case <-ctx.Done():
+					fmt.Println("context is cancelled, go home.")
+					return
+				default:
+				}
+			}()
+
+		}
+	}(baseCtx)
+
+	// HTTP service running in this program as well. The valve context is set
+	// as a base context on the server listener at the point where we instantiate
+	// the server - look lower.
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("sup"))
+	})
+
+	r.Get("/slow", func(w http.ResponseWriter, r *http.Request) {
+
+		valve.Context(r.Context()).Open()
+		defer valve.Context(r.Context()).Close()
+
+		select {
+		case <-valve.Context(r.Context()).Stop():
+			fmt.Println("valve is closed. finish up..")
+
+		case <-time.After(5 * time.Second):
+			// The above channel simulates some hard work.
+			// We want this handler to complete successfully during a shutdown signal,
+			// so consider the work here as some background routine to fetch a long running
+			// search query to find as many results as possible, but, instead we cut it short
+			// and respond with what we have so far. How a shutdown is handled is entirely
+			// up to the developer, as some code blocks are preemptable, and others are not.
+			time.Sleep(5 * time.Second)
+		}
+
+		w.Write([]byte(fmt.Sprintf("all done.\n")))
+	})
+
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt)
+	// go func() {
+	// 	for sig := range c {
+	// 		// sig is a ^C, handle it
+	// 		valv.Shutdown()
+	// 		os.Exit(1)
+	// 	}
+	// }()
+	// http.ListenAndServe(":3333", chi.WithBaseContext(r, baseCtx))
+
+	srv := &graceful.Server{
+		Timeout: 20 * time.Second,
+		Server:  &http.Server{Addr: ":3333", Handler: chi.WithBaseContext(r, baseCtx)},
+	}
+	srv.BeforeShutdown = func() bool {
+		fmt.Println("shutting down..")
+		err := valv.Shutdown(srv.Timeout)
+		if err != nil {
+			fmt.Println("Shutdown error -", err)
+		}
+		// the app code has stopped here now, and so this would be a good place
+		// to close up any db and other service connections, etc.
+		return true
+	}
+	srv.ListenAndServe()
+}
