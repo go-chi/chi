d573d1cfc43e866ab2c987c443468380a4e0cd5f
Nikola Kovacs <nikola.kovacs@gmail.com>
2017-02-14 11:35:15 +0100

    Fix example in middleware.Profiler godoc

--- middleware/profiler.go
+++ middleware/profiler.go
@@ -14,7 +14,7 @@ import (
 //  func MyService() http.Handler {
 //    r := chi.NewRouter()
 //    // ..middlewares
-//    r.Mount("/debug", profiler.Router())
+//    r.Mount("/debug", middleware.Profiler())
 //    // ..routes
 //    return r
 //  }
