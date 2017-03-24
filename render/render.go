af974076824d15191fdf3a0213aee407d69fb12a
Peter Kieltyka <peter.kieltyka@pressly.com>
2017-03-24 13:46:21 -0400

    Rewrite of chi/render pkg, introducing Binder and Renderer interfaces for handling well-defined request/response payloads

--- render/render.go
+++ render/render.go
@@ -1,202 +1,120 @@
 package render
 
 import (
-	"bytes"
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
 	"net/http"
 	"reflect"
 )
 
-// Respond handles streaming JSON and XML responses, automatically setting the
-// Content-Type based on request headers. It will default to a JSON response.
-func Respond(w http.ResponseWriter, r *http.Request, v interface{}) {
-	// Present the object.
-	if presenter, ok := r.Context().Value(presenterCtxKey).(Presenter); ok {
-		r, v = presenter.Present(r, v)
-	}
+type Renderer interface {
+	Render(w http.ResponseWriter, r *http.Request) error
+}
 
-	switch reflect.TypeOf(v).Kind() {
-	case reflect.Chan:
-		switch getResponseContentType(r) {
-		case ContentTypeEventStream:
-			channelEventStream(w, r, v)
-			return
-		default:
-			v = channelIntoSlice(w, r, v)
+type Binder interface {
+	Bind(r *http.Request) error
+}
+
+func Bind(r *http.Request, v Binder) error {
+	if err := Decode(r, v); err != nil {
+		return err
+	}
+	return binder(r, v)
+}
+
+func Render(w http.ResponseWriter, r *http.Request, v Renderer) error {
+	if err := renderer(w, r, v); err != nil {
+		return err
+	}
+	Respond(w, r, v)
+	return nil
+}
+
+func RenderList(w http.ResponseWriter, r *http.Request, l []Renderer) error {
+	for _, v := range l {
+		if err := renderer(w, r, v); err != nil {
+			return err
 		}
 	}
-
-	// Format data based on Content-Type.
-	switch getResponseContentType(r) {
-	case ContentTypeJSON:
-		JSON(w, r, v)
-	case ContentTypeXML:
-		XML(w, r, v)
-	default:
-		JSON(w, r, v)
-	}
+	Respond(w, r, l)
+	return nil
 }
 
-// PlainText writes a string to the response, setting the Content-Type as
-// text/plain.
-func PlainText(w http.ResponseWriter, r *http.Request, v string) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	if status, ok := r.Context().Value(statusCtxKey).(int); ok {
-		w.WriteHeader(status)
-	}
-	w.Write([]byte(v))
-}
-
-// Data writes raw bytes to the response, setting the Content-Type as
-// application/octet-stream.
-func Data(w http.ResponseWriter, r *http.Request, v []byte) {
-	w.Header().Set("Content-Type", "application/octet-stream")
-	if status, ok := r.Context().Value(statusCtxKey).(int); ok {
-		w.WriteHeader(status)
-	}
-	w.Write(v)
-}
-
-// HTML writes a string to the response, setting the Content-Type as text/html.
-func HTML(w http.ResponseWriter, r *http.Request, v string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if status, ok := r.Context().Value(statusCtxKey).(int); ok {
-		w.WriteHeader(status)
-	}
-	w.Write([]byte(v))
-}
-
-// JSON marshals 'v' to JSON, automatically escaping HTML and setting the
-// Content-Type as application/json.
-func JSON(w http.ResponseWriter, r *http.Request, v interface{}) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	if status, ok := r.Context().Value(statusCtxKey).(int); ok {
-		w.WriteHeader(status)
+// Executed top-down
+func renderer(w http.ResponseWriter, r *http.Request, v Renderer) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
 	}
 
-	enc := json.NewEncoder(w)
-	enc.SetEscapeHTML(true)
-	if err := enc.Encode(v); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-// XML marshals 'v' to XML, setting the Content-Type as application/xml. It
-// will automatically prepend a generic XML header (see encoding/xml.Header) if
-// one is not found in the first 100 bytes of 'v'.
-func XML(w http.ResponseWriter, r *http.Request, v interface{}) {
-	b, err := xml.Marshal(v)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	// We call it top-down.
+	if err := v.Render(w, r); err != nil {
+		return err
 	}
 
-	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-	if status, ok := r.Context().Value(statusCtxKey).(int); ok {
-		w.WriteHeader(status)
+	// We're done if the Renderer isn't a struct object
+	if rv.Kind() != reflect.Struct {
+		return nil
 	}
 
-	// Try to find <?xml header in first 100 bytes (just in case there're some XML comments).
-	findHeaderUntil := len(b)
-	if findHeaderUntil > 100 {
-		findHeaderUntil = 100
-	}
-	if !bytes.Contains(b[:findHeaderUntil], []byte("<?xml")) {
-		// No header found. Print it out first.
-		w.Write([]byte(xml.Header))
-	}
+	// For structs, we call Render on each field that implements Renderer
+	for i := 0; i < rv.NumField(); i++ {
+		f := rv.Field(i)
+		if f.Type().Implements(rendererType) {
 
-	w.Write(b)
-}
-
-// NoContent returns a HTTP 204 "No Content" response.
-func NoContent(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(204)
-}
-
-func channelEventStream(w http.ResponseWriter, r *http.Request, v interface{}) {
-	if reflect.TypeOf(v).Kind() != reflect.Chan {
-		panic(fmt.Sprintf("render.EventStream() expects channel, not %v", reflect.TypeOf(v).Kind()))
-	}
-
-	w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.WriteHeader(200)
-
-	ctx := r.Context()
-	for {
-		switch chosen, recv, ok := reflect.Select([]reflect.SelectCase{
-			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())},
-			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(v)},
-		}); chosen {
-		case 0: // equivalent to: case <-ctx.Done()
-			w.Write([]byte("event: error\ndata: {\"error\":\"Server Timeout\"}\n\n"))
-			return
-
-		default: // equivalent to: case v, ok := <-stream
-			if !ok {
-				w.Write([]byte("event: EOF\n\n"))
-				return
-			}
-			v := recv.Interface()
-
-			// Present each channel item.
-			if presenter, ok := r.Context().Value(presenterCtxKey).(Presenter); ok {
-				r, v = presenter.Present(r, v)
-			}
-
-			// TODO: Can't use json Encoder - it panics on bufio.Flush(). Why?!
-			// enc := json.NewEncoder(w)
-			// enc.Encode(v.Interface())
-			bytes, err := json.Marshal(v)
-			if err != nil {
-				w.Write([]byte(fmt.Sprintf("event: error\ndata: {\"error\":\"%v\"}\n\n", err)))
-				if f, ok := w.(http.Flusher); ok {
-					f.Flush()
-				}
+			if f.IsNil() {
 				continue
 			}
-			w.Write([]byte(fmt.Sprintf("event: data\ndata: %s\n\n", bytes)))
-			if f, ok := w.(http.Flusher); ok {
-				f.Flush()
+
+			fv := f.Interface().(Renderer)
+			if err := renderer(w, r, fv); err != nil {
+				return err
+			}
+
+		}
+	}
+
+	return nil
+}
+
+// Executed bottom-up
+func binder(r *http.Request, v Binder) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+
+	// Call Binder on non-struct types right away
+	if rv.Kind() != reflect.Struct {
+		return v.Bind(r)
+	}
+
+	// For structs, we call Bind on each field that implements Binder
+	for i := 0; i < rv.NumField(); i++ {
+		f := rv.Field(i)
+		if f.Type().Implements(binderType) {
+
+			if f.IsNil() {
+				continue
+			}
+
+			fv := f.Interface().(Binder)
+			if err := binder(r, fv); err != nil {
+				return err
 			}
 		}
 	}
-}
 
-// channelIntoSlice buffers channel data into a slice.
-func channelIntoSlice(w http.ResponseWriter, r *http.Request, from interface{}) interface{} {
-	ctx := r.Context()
-
-	var to []interface{}
-	for {
-		switch chosen, recv, ok := reflect.Select([]reflect.SelectCase{
-			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())},
-			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(from)},
-		}); chosen {
-		case 0: // equivalent to: case <-ctx.Done()
-			http.Error(w, "Server Timeout", 504)
-			return nil
-
-		default: // equivalent to: case v, ok := <-stream
-			if !ok {
-				return to
-			}
-			v := recv.Interface()
-
-			// Present each channel item.
-			if presenter, ok := r.Context().Value(presenterCtxKey).(Presenter); ok {
-				r, v = presenter.Present(r, v)
-			}
-
-			to = append(to, v)
-		}
+	// We call it bottom-up
+	if err := v.Bind(r); err != nil {
+		return err
 	}
+
+	return nil
 }
+
+var (
+	rendererType = reflect.TypeOf(new(Renderer)).Elem()
+	binderType   = reflect.TypeOf(new(Binder)).Elem()
+)
 
 // contextKey is a value for use with context.WithValue. It's used as
 // a pointer so it fits in an interface{} without allocation. This technique
