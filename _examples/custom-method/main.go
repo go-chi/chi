d806569952e5c14126a155fe66cf102139d11979
Peter Kieltyka <peter@pressly.com>
2017-10-10 17:28:23 -0400

    New chi.RegisterMethod(method) to add support for custom HTTP methods (#271)

---
+++ _examples/custom-method/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func init() {
+	chi.RegisterMethod("LINK")
+	chi.RegisterMethod("UNLINK")
+	chi.RegisterMethod("WOOHOO")
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	r.MethodFunc("LINK", "/link", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("custom link method"))
+	})
+	r.MethodFunc("WOOHOO", "/woo", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("custom woohoo method"))
+	})
+	r.HandleFunc("/everything", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("capturing all standard http methods, as well as LINK, UNLINK and WOOHOO"))
+	})
+	http.ListenAndServe(":3333", r)
+}
