35d4234564fc1a3ca877483751eba838e7dc1c4e
Igor Varavko <igor.varavko@gmail.com>
2017-10-20 21:34:38 +0300

    Fix 'elasped' misspelling of 'elapsed'

--- _examples/logging/main.go
+++ _examples/logging/main.go
@@ -103,7 +103,7 @@ type StructuredLoggerEntry struct {
 func (l *StructuredLoggerEntry) Write(status, bytes int, elapsed time.Duration) {
 	l.Logger = l.Logger.WithFields(logrus.Fields{
 		"resp_status": status, "resp_bytes_length": bytes,
-		"resp_elasped_ms": float64(elapsed.Nanoseconds()) / 1000000.0,
+		"resp_elapsed_ms": float64(elapsed.Nanoseconds()) / 1000000.0,
 	})
 
 	l.Logger.Infoln("request complete")
