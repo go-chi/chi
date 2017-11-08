c584fb8b92f9876dec890470db1878da4a355a1a
Peter Kieltyka <peter.kieltyka@pressly.com>
2017-11-08 17:18:54 -0500

    middleware: new SetHeader handler to set response header key/value

--- middleware/content_type.go
+++ middleware/content_type.go
@@ -5,6 +5,17 @@ import (
 	"strings"
 )
 
+// SetHeader is a convenience handler to set a response header key/value
+func SetHeader(key, value string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set(key, value)
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
+
 // AllowContentType enforces a whitelist of request Content-Types otherwise responds
 // with a 415 Unsupported Media Type status.
 func AllowContentType(contentTypes ...string) func(next http.Handler) http.Handler {
