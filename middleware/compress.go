50e0bd7efe608dfa2645fca46b36042854b773a3
aaharu <aaharu@hotmail.com>
2019-04-13 21:34:02 +0900

    middleware: set Vary header if compressed

--- middleware/compress.go
+++ middleware/compress.go
@@ -327,6 +327,7 @@ func (cw *compressResponseWriter) WriteHeader(code int) {
 
 	if cw.encoding != "" {
 		cw.Header().Set("Content-Encoding", cw.encoding)
+		cw.Header().Set("Vary", "Accept-Encoding")
 
 		// The content-length after compression is unknown
 		cw.Header().Del("Content-Length")
