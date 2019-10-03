906b567ebae89be14ccf532cdf29dd57645adc4a
Peter Kieltyka <peter.kieltyka@gmail.com>
2019-10-03 06:24:41 -0400

    middleware: fix routing issue in throttler

--- middleware/throttle.go
+++ middleware/throttle.go
@@ -16,7 +16,9 @@ var (
 )
 
 // Throttle is a middleware that limits number of currently processed requests
-// at a time.
+// at a time across all users. Note: Throttle is not a rate-limiter per user,
+// instead it just puts a ceiling on the number of currentl in-flight requests
+// being processed from the point from where the Throttle middleware is mounted.
 func Throttle(limit int) func(http.Handler) http.Handler {
 	return ThrottleBacklog(limit, 0, defaultBacklogTimeout)
 }
@@ -47,12 +49,48 @@ func ThrottleBacklog(limit int, backlogLimit int, backlogTimeout time.Duration) 
 		t.backlogTokens <- token{}
 	}
 
-	fn := func(h http.Handler) http.Handler {
-		t.h = h
-		return &t
-	}
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
 
-	return fn
+			select {
+
+			case <-ctx.Done():
+				http.Error(w, errContextCanceled, http.StatusServiceUnavailable)
+				return
+
+			case btok := <-t.backlogTokens:
+				timer := time.NewTimer(t.backlogTimeout)
+
+				defer func() {
+					t.backlogTokens <- btok
+				}()
+
+				select {
+				case <-timer.C:
+					http.Error(w, errTimedOut, http.StatusServiceUnavailable)
+					return
+				case <-ctx.Done():
+					timer.Stop()
+					http.Error(w, errContextCanceled, http.StatusServiceUnavailable)
+					return
+				case tok := <-t.tokens:
+					defer func() {
+						timer.Stop()
+						t.tokens <- tok
+					}()
+					next.ServeHTTP(w, r)
+				}
+				return
+
+			default:
+				http.Error(w, errCapacityExceeded, http.StatusServiceUnavailable)
+				return
+			}
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
 
 // token represents a request that is being processed.
@@ -60,44 +98,7 @@ type token struct{}
 
 // throttler limits number of currently processed requests at a time.
 type throttler struct {
-	h              http.Handler
 	tokens         chan token
 	backlogTokens  chan token
 	backlogTimeout time.Duration
-}
-
-// ServeHTTP is the primary throttler request handler
-func (t *throttler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	select {
-	case <-ctx.Done():
-		http.Error(w, errContextCanceled, http.StatusServiceUnavailable)
-		return
-	case btok := <-t.backlogTokens:
-		timer := time.NewTimer(t.backlogTimeout)
-
-		defer func() {
-			t.backlogTokens <- btok
-		}()
-
-		select {
-		case <-timer.C:
-			http.Error(w, errTimedOut, http.StatusServiceUnavailable)
-			return
-		case <-ctx.Done():
-			timer.Stop()
-			http.Error(w, errContextCanceled, http.StatusServiceUnavailable)
-			return
-		case tok := <-t.tokens:
-			defer func() {
-				timer.Stop()
-				t.tokens <- tok
-			}()
-			t.h.ServeHTTP(w, r)
-		}
-		return
-	default:
-		http.Error(w, errCapacityExceeded, http.StatusServiceUnavailable)
-		return
-	}
 }
