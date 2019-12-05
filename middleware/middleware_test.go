10fe98186c07cc168c6b9c1377c8b9841b988e88
vasayxtx <vasayxtx@gmail.com>
2019-12-05 14:30:07 +0300

    Make assert* funcs in tests as test-helpers

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -136,18 +136,21 @@ func testRequestNoRedirect(t *testing.T, ts *httptest.Server, method, path strin
 }
 
 func assertNoError(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
 		t.Fatalf("expecting no error")
 	}
 }
 
 func assertError(t *testing.T, err error) {
+	t.Helper()
 	if err == nil {
 		t.Fatalf("expecting error")
 	}
 }
 
 func assertEqual(t *testing.T, a, b interface{}) {
+	t.Helper()
 	if !reflect.DeepEqual(a, b) {
 		t.Fatalf("expecting values to be equal but got: '%v' and '%v'", a, b)
 	}
