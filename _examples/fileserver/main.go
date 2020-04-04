014d4cc44cf77a43cd41630e87afc61cf3ff4785
Peter Kieltyka <peter.kieltyka@gmail.com>
2020-04-04 14:53:08 -0400

    examples: update fileserver example with much more flexible impl

--- _examples/fileserver/main.go
+++ _examples/fileserver/main.go
@@ -1,3 +1,23 @@
+//
+// FileServer
+// ===========
+// This example demonstrates how to serve static files from your filesystem.
+//
+//
+// Boot the server:
+// ----------------
+// $ go run main.go
+//
+// Client requests:
+// ----------------
+// $ curl http://localhost:3333/files/
+// <pre>
+// <a href="notes.txt">notes.txt</a>
+// </pre>
+//
+// $ curl http://localhost:3333/files/notes.txt
+// Notessszzz
+//
 package main
 
 import (
@@ -7,34 +27,33 @@ import (
 	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	basePath := "/sub"
-
-	r.Route(basePath, func(root chi.Router) {
-		root.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("hi"))
-		})
-
-		workDir, _ := os.Getwd()
-		filesDir := filepath.Join(workDir, "files")
-		FileServer(root, basePath, "/files", http.Dir(filesDir))
+	// Index handler
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
 	})
+
+	// Create a route along /files that will serve contents from
+	// the ./data/ folder.
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "data"))
+	FileServer(r, "/files", filesDir)
 
 	http.ListenAndServe(":3333", r)
 }
 
 // FileServer conveniently sets up a http.FileServer handler to serve
 // static files from a http.FileSystem.
-func FileServer(r chi.Router, basePath string, path string, root http.FileSystem) {
+func FileServer(r chi.Router, path string, root http.FileSystem) {
 	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit URL parameters.")
+		panic("FileServer does not permit any URL parameters.")
 	}
-
-	fs := http.StripPrefix(basePath+path, http.FileServer(root))
 
 	if path != "/" && path[len(path)-1] != '/' {
 		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
@@ -42,7 +61,10 @@ func FileServer(r chi.Router, basePath string, path string, root http.FileSystem
 	}
 	path += "*"
 
-	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
 		fs.ServeHTTP(w, r)
-	}))
+	})
 }
