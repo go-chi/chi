dd96695896c56928b2a7df4c6e2e8d3216a37160
Julian R Figueroa <jfigueroa@twilio.com>
2020-05-29 18:58:48 -0500

    ResourceID as pattern

--- context_test.go
+++ context_test.go
@@ -13,23 +13,24 @@ import "testing"
 // (resources_controller.go)
 // r.Route("/", func(r chi.Router) {
 // 	r.Get("/{resource_id}", getResource())
+// 	other routes...
 // }
 //
-// The route pattern could be "/v1/resources/*/resource_id" instead of "/v1/resources/resource_id"
+// The route pattern could be "/v1/resources/*/{resource_id}" instead of "/v1/resources/{resource_id}"
 // This test makes sure wildcards are removed correctly.
 func TestRoutePattern(t *testing.T) {
 	routePatterns := []string{
 		"/v1",
 		"/resources/*",
-		"/resource_id",
+		"/{resource_id}",
 	}
 
 	x := &Context{
 		RoutePatterns: routePatterns,
 	}
 
-	if p := x.RoutePattern(); p != "/v1/resources/resource_id" {
-		t.Fatal("unexpected route path: " + p)
+	if p := x.RoutePattern(); p != "/v1/resources/{resource_id}" {
+		t.Fatal("unexpected route pattern: " + p)
 	}
 
 	x.RoutePatterns = []string{
@@ -37,12 +38,12 @@ func TestRoutePattern(t *testing.T) {
 		"/resources/*",
 		// Additional wildcard, depending on the router structure of the user
 		"/*",
-		"/resource_id",
+		"/{resource_id}",
 	}
 
-	// Correctly removes wildcards instead of "/v1/resources/*/resource_id"
-	if p := x.RoutePattern(); p != "/v1/resources/resource_id" {
-		t.Fatal("unexpected route path: " + p)
+	// Correctly removes wildcards instead of "/v1/resources/*/{resource_id}"
+	if p := x.RoutePattern(); p != "/v1/resources/{resource_id}" {
+		t.Fatal("unexpected route pattern: " + p)
 	}
 
 	x.RoutePatterns = []string{
@@ -52,11 +53,11 @@ func TestRoutePattern(t *testing.T) {
 		"/*",
 		"/*",
 		"/*",
-		"/resource_id/*", // And trailing wildcard
+		"/{resource_id}/*", // And trailing wildcard
 	}
 
-	// Correctly removes wildcards instead of "/v1/resources/*/*/resource_id/*"
-	if p := x.RoutePattern(); p != "/v1/resources/resource_id" {
-		t.Fatal("unexpected route path: " + p)
+	// Correctly removes wildcards instead of "/v1/resources/*/*/{resource_id}/*"
+	if p := x.RoutePattern(); p != "/v1/resources/{resource_id}" {
+		t.Fatal("unexpected route pattern: " + p)
 	}
 }
