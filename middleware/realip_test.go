2a141d012c728debd410ab5c924d95e71b10b6d1
Martin Tournoij <martin@arp242.net>
2020-06-12 13:24:41 +0800

    Add port number to RealIP middleware
    Fixes #453
    
    Downside: this is not entirely backwards compatible; e.g. my app will
    actually break because it *assumes* you're running it behind a proxy and
    won't remove the port. Then again, it already breaks for people not
    using a proxy, but in certain scenarios (e.g. some closed source app)
    that will probably never really happen.
    
    So ... maybe something for v5? Just thought I'd open a PR in any case.

--- middleware/realip_test.go
+++ middleware/realip_test.go
@@ -8,50 +8,69 @@ import (
 	"github.com/go-chi/chi"
 )
 
-func TestXRealIP(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/", nil)
-	req.Header.Add("X-Real-IP", "100.100.100.100")
-	w := httptest.NewRecorder()
-
-	r := chi.NewRouter()
-	r.Use(RealIP)
-
-	realIP := ""
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		realIP = r.RemoteAddr
-		w.Write([]byte("Hello World"))
-	})
-	r.ServeHTTP(w, req)
-
-	if w.Code != 200 {
-		t.Fatal("Response Code should be 200")
+func TestRealIP(t *testing.T) {
+	tests := []struct {
+		name       string
+		remoteAddr string
+		header     http.Header
+		want       string
+	}{
+		{
+			"remote-addr",
+			"1.1.1.1:42",
+			nil,
+			"1.1.1.1:42",
+		},
+		{
+			"x-real-ip",
+			"1.1.1.1",
+			http.Header{"X-Real-Ip": {"100.100.100.100"}},
+			"100.100.100.100:0",
+		},
+		{
+			"x-real-ip-6",
+			"2001:beef::0",
+			http.Header{"X-Real-Ip": {"2001:dead::0"}},
+			"[2001:dead::0]:0",
+		},
+		{
+			"x-forwarded-for",
+			"1.1.1.1",
+			http.Header{"X-Forwarded-For": {"100.100.100.100"}},
+			"100.100.100.100:0",
+		},
+		{
+			"x-forwarded-for-6",
+			"2001:beef::0",
+			http.Header{"X-Forwarded-For": {"2001:dead::0"}},
+			"[2001:dead::0]:0",
+		},
 	}
 
-	if realIP != "100.100.100.100" {
-		t.Fatal("Test get real IP error.")
-	}
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "/", nil)
+			req.RemoteAddr = tt.remoteAddr
+			req.Header = tt.header
 
-func TestXForwardForIP(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/", nil)
-	req.Header.Add("X-Forwarded-For", "100.100.100.100")
-	w := httptest.NewRecorder()
+			w := httptest.NewRecorder()
+			r := chi.NewRouter()
+			r.Use(RealIP)
 
-	r := chi.NewRouter()
-	r.Use(RealIP)
+			realIP := ""
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				realIP = r.RemoteAddr
+				w.Write([]byte("Hello World"))
+			})
+			r.ServeHTTP(w, req)
 
-	realIP := ""
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		realIP = r.RemoteAddr
-		w.Write([]byte("Hello World"))
-	})
-	r.ServeHTTP(w, req)
+			if w.Code != 200 {
+				t.Fatalf("wrong response code: %d; wanted 200", w.Code)
+			}
 
-	if w.Code != 200 {
-		t.Fatal("Response Code should be 200")
-	}
-
-	if realIP != "100.100.100.100" {
-		t.Fatal("Test get real IP error.")
+			if realIP != tt.want {
+				t.Fatalf("wrong IP: %q; wanted %q", realIP, tt.want)
+			}
+		})
 	}
 }
