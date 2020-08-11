fc49a78157c362da539a6ff2bf148546bdcea7f9
Vojtech Vitek <vojtech.vitek@gmail.com>
2020-08-11 14:51:01 +0200

    Improve middleware.NoCache() performance

--- middleware/nocache.go
+++ middleware/nocache.go
@@ -11,14 +11,6 @@ import (
 // Unix epoch time
 var epoch = time.Unix(0, 0).Format(time.RFC1123)
 
-// Taken from https://github.com/mytrile/nocache
-var noCacheHeaders = map[string]string{
-	"Expires":         epoch,
-	"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
-	"Pragma":          "no-cache",
-	"X-Accel-Expires": "0",
-}
-
 var etagHeaders = []string{
 	"ETag",
 	"If-Modified-Since",
@@ -41,15 +33,15 @@ func NoCache(h http.Handler) http.Handler {
 
 		// Delete any ETag headers that may have been set
 		for _, v := range etagHeaders {
-			if r.Header.Get(v) != "" {
-				r.Header.Del(v)
-			}
+			r.Header.Del(v)
 		}
 
-		// Set our NoCache headers
-		for k, v := range noCacheHeaders {
-			w.Header().Set(k, v)
-		}
+		// Set our NoCache headers. Taken from https://github.com/mytrile/nocache.
+		header := w.Header()
+		header.Set("Expires", epoch)
+		header.Set("Cache-Control", "no-cache, no-store, no-transform, must-revalidate, private, max-age=0")
+		header.Set("Pragma", "no-cache")
+		header.Set("X-Accel-Expires", "0")
 
 		h.ServeHTTP(w, r)
 	}
