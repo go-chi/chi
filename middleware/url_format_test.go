73938b5d1ab159050b9822e620e5e64109d229cf
Derek Bassett <derekbassett@users.noreply.github.com>
2020-12-05 08:36:39 -0700

    Chops the Suffix rather than the Prefix (#531)
    * Chop the Suffix rather then the Prefix so behavior matches how the middleware is described in the comments.
    
    * goimports

---
+++ middleware/url_format_test.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+)
+
+func TestUrlFormat(t *testing.T) {
+	r := chi.NewRouter()
+
+	r.Use(URLFormat)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		w.Write([]byte("nothing here"))
+	})
+
+	r.Route("/samples/articles/samples.{articleID}", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			articleID := chi.URLParam(r, "articleID")
+			w.Write([]byte(articleID))
+		})
+	})
+
+	r.Route("/articles/{articleID}", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			articleID := chi.URLParam(r, "articleID")
+			w.Write([]byte(articleID))
+		})
+	})
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	if _, resp := testRequest(t, ts, "GET", "/articles/1.json", nil); resp != "1" {
+		t.Fatalf(resp)
+	}
+	if _, resp := testRequest(t, ts, "GET", "/articles/1.xml", nil); resp != "1" {
+		t.Fatalf(resp)
+	}
+	if _, resp := testRequest(t, ts, "GET", "/samples/articles/samples.1.json", nil); resp != "1" {
+		t.Fatalf(resp)
+	}
+	if _, resp := testRequest(t, ts, "GET", "/samples/articles/samples.1.xml", nil); resp != "1" {
+		t.Fatalf(resp)
+	}
+}
