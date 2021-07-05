a63dd1f8a9ea3f82bf46c24967f077ee7c764ef8
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-07-05 09:13:52 -0400

    _examples: new graceful example, better

--- _examples/graceful/main.go
+++ _examples/graceful/main.go
@@ -3,58 +3,61 @@ package main
 import (
 	"context"
 	"fmt"
-	"net"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/valve"
 )
 
 func main() {
+	// The HTTP Server
+	server := &http.Server{Addr: "0.0.0.0:3333", Handler: service()}
 
-	// Our graceful valve shut-off package to manage code preemption and
-	// shutdown signaling.
-	valv := valve.New()
-	baseCtx := valv.Context()
+	// Server run context
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
 
-	// Example of a long running background worker thing..
-	go func(ctx context.Context) {
-		for {
-			<-time.After(1 * time.Second)
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
 
-			func() {
-				valve.Lever(ctx).Open()
-				defer valve.Lever(ctx).Close()
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
 
-				// actual code doing stuff..
-				fmt.Println("tick..")
-				time.Sleep(2 * time.Second)
-				// end-logic
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
 
-				// signal control..
-				select {
-				case <-valve.Lever(ctx).Stop():
-					fmt.Println("valve is closed")
-					return
-
-				case <-ctx.Done():
-					fmt.Println("context is cancelled, go home.")
-					return
-				default:
-				}
-			}()
-
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
 		}
-	}(baseCtx)
+		serverStopCtx()
+	}()
 
-	// HTTP service running in this program as well. The valve context is set
-	// as a base context on the server listener at the point where we instantiate
-	// the server - look lower.
+	// Run the server
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+}
+
+func service() http.Handler {
 	r := chi.NewRouter()
+
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
@@ -63,57 +66,17 @@ func main() {
 	})
 
 	r.Get("/slow", func(w http.ResponseWriter, r *http.Request) {
-
-		valve.Lever(r.Context()).Open()
-		defer valve.Lever(r.Context()).Close()
-
-		select {
-		case <-valve.Lever(r.Context()).Stop():
-			fmt.Println("valve is closed. finish up..")
-
-		case <-time.After(5 * time.Second):
-			// The above channel simulates some hard work.
-			// We want this handler to complete successfully during a shutdown signal,
-			// so consider the work here as some background routine to fetch a long running
-			// search query to find as many results as possible, but, instead we cut it short
-			// and respond with what we have so far. How a shutdown is handled is entirely
-			// up to the developer, as some code blocks are preemptable, and others are not.
-			time.Sleep(5 * time.Second)
-		}
+		// Simulates some hard work.
+		//
+		// We want this handler to complete successfully during a shutdown signal,
+		// so consider the work here as some background routine to fetch a long running
+		// search query to find as many results as possible, but, instead we cut it short
+		// and respond with what we have so far. How a shutdown is handled is entirely
+		// up to the developer, as some code blocks are preemptable, and others are not.
+		time.Sleep(5 * time.Second)
 
 		w.Write([]byte(fmt.Sprintf("all done.\n")))
 	})
 
-	srv := http.Server{Addr: ":3333", Handler: r}
-	srv.BaseContext = func(_ net.Listener) context.Context {
-		return baseCtx
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		for range c {
-			// sig is a ^C, handle it
-			fmt.Println("shutting down..")
-
-			// first valv
-			valv.Shutdown(20 * time.Second)
-
-			// create context with timeout
-			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-			defer cancel()
-
-			// start http shutdown
-			srv.Shutdown(ctx)
-
-			// verify, in worst case call cancel via defer
-			select {
-			case <-time.After(21 * time.Second):
-				fmt.Println("not all connections done")
-			case <-ctx.Done():
-
-			}
-		}
-	}()
-	srv.ListenAndServe()
+	return r
 }
