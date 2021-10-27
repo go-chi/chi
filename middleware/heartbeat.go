ecd468d8a624902edbc55b28b343631ef855843b
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-10-27 07:10:07 -0400

    middleware: Heartbeat middleware to respond to HEAD methods as well

--- middleware/heartbeat.go
+++ middleware/heartbeat.go
@@ -12,7 +12,7 @@ import (
 func Heartbeat(endpoint string) func(http.Handler) http.Handler {
 	f := func(h http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			if r.Method == "GET" && strings.EqualFold(r.URL.Path, endpoint) {
+			if (r.Method == "GET" || r.Method == "HEAD") && strings.EqualFold(r.URL.Path, endpoint) {
 				w.Header().Set("Content-Type", "text/plain")
 				w.WriteHeader(http.StatusOK)
 				w.Write([]byte("."))
