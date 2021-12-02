91a25a1a650dca6c6903fd33798394a1cbd2f0fa
evgenyKharitonov <evgeny.kharitonov@tailorbrands.com>
2021-12-02 19:02:09 +0200

    support ip chain without spaces in xForwarded header

--- middleware/realip_test.go
+++ middleware/realip_test.go
@@ -33,26 +33,35 @@ func TestXRealIP(t *testing.T) {
 }
 
 func TestXForwardForIP(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/", nil)
-	req.Header.Add("X-Forwarded-For", "100.100.100.100")
-	w := httptest.NewRecorder()
+	xForwardedForIPs := []string{
+		"100.100.100.100",
+		"100.100.100.100, 200.200.200.200",
+		"100.100.100.100,200.200.200.200",
+	}
 
 	r := chi.NewRouter()
 	r.Use(RealIP)
 
-	realIP := ""
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		realIP = r.RemoteAddr
-		w.Write([]byte("Hello World"))
-	})
-	r.ServeHTTP(w, req)
+	for _, v := range xForwardedForIPs {
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Add("X-Forwarded-For", v)
 
-	if w.Code != 200 {
-		t.Fatal("Response Code should be 200")
-	}
+		w := httptest.NewRecorder()
 
-	if realIP != "100.100.100.100" {
-		t.Fatal("Test get real IP error.")
+		realIP := ""
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			realIP = r.RemoteAddr
+			w.Write([]byte("Hello World"))
+		})
+		r.ServeHTTP(w, req)
+
+		if w.Code != 200 {
+			t.Fatal("Response Code should be 200")
+		}
+
+		if realIP != "100.100.100.100" {
+			t.Fatal("Test get real IP error.")
+		}
 	}
 }
 
