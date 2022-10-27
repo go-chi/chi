42a41a8f677355207d4a9ef9594713a240e94c7d
Peter Kieltyka <peter.kieltyka@gmail.com>
2022-10-27 07:54:02 -0400

    update comment in source

--- chi.go
+++ chi.go
@@ -1,29 +1,29 @@
-//
 // Package chi is a small, idiomatic and composable router for building HTTP services.
 //
-// chi requires Go 1.10 or newer.
+// chi requires Go 1.14 or newer.
 //
 // Example:
-//  package main
 //
-//  import (
-//  	"net/http"
+//	package main
 //
-//  	"github.com/go-chi/chi/v5"
-//  	"github.com/go-chi/chi/v5/middleware"
-//  )
+//	import (
+//		"net/http"
 //
-//  func main() {
-//  	r := chi.NewRouter()
-//  	r.Use(middleware.Logger)
-//  	r.Use(middleware.Recoverer)
+//		"github.com/go-chi/chi/v5"
+//		"github.com/go-chi/chi/v5/middleware"
+//	)
 //
-//  	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-//  		w.Write([]byte("root."))
-//  	})
+//	func main() {
+//		r := chi.NewRouter()
+//		r.Use(middleware.Logger)
+//		r.Use(middleware.Recoverer)
 //
-//  	http.ListenAndServe(":3333", r)
-//  }
+//		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+//			w.Write([]byte("root."))
+//		})
+//
+//		http.ListenAndServe(":3333", r)
+//	}
 //
 // See github.com/go-chi/chi/_examples/ for more in-depth examples.
 //
@@ -47,12 +47,12 @@
 // placeholder which will match / characters.
 //
 // Examples:
-//  "/user/{name}" matches "/user/jsmith" but not "/user/jsmith/info" or "/user/jsmith/"
-//  "/user/{name}/info" matches "/user/jsmith/info"
-//  "/page/*" matches "/page/intro/latest"
-//  "/page/*/index" also matches "/page/intro/latest"
-//  "/date/{yyyy:\\d\\d\\d\\d}/{mm:\\d\\d}/{dd:\\d\\d}" matches "/date/2017/04/01"
 //
+//	"/user/{name}" matches "/user/jsmith" but not "/user/jsmith/info" or "/user/jsmith/"
+//	"/user/{name}/info" matches "/user/jsmith/info"
+//	"/page/*" matches "/page/intro/latest"
+//	"/page/{other}/index" also matches "/page/intro/latest"
+//	"/date/{yyyy:\\d\\d\\d\\d}/{mm:\\d\\d}/{dd:\\d\\d}" matches "/date/2017/04/01"
 package chi
 
 import "net/http"
