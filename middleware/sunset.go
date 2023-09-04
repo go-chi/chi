223fc37df6dd77c7f2010a38eefa712bdfdc9a20
Saddam H <thedevsaddam@gmail.com>
2023-09-04 21:20:03 +0600

    refactor: append links instead of restting previous one
    Respect previously added links as this header can be used by other
    components

--- middleware/sunset.go
+++ middleware/sunset.go
@@ -7,7 +7,7 @@ import (
 
 // Sunset set Deprecation/Sunset header to response
 // This can be used to enable Sunset in a route or a route group
-// For more at: https://www.rfc-editor.org/rfc/rfc8594.html
+// For more: https://www.rfc-editor.org/rfc/rfc8594.html
 func Sunset(sunsetAt time.Time, links ...string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -15,11 +15,7 @@ func Sunset(sunsetAt time.Time, links ...string) func(http.Handler) http.Handler
 				w.Header().Set("Sunset", sunsetAt.Format(http.TimeFormat))
 				w.Header().Set("Deprecation", sunsetAt.Format(http.TimeFormat))
 
-				for i, link := range links {
-					if i == 0 {
-						w.Header().Set("Link", link)
-						continue
-					}
+				for _, link := range links {
 					w.Header().Add("Link", link)
 				}
 			}
