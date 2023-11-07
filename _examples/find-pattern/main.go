1f4b3131322d43801247f097980e92336a8a26bf
jriddle <jriddle@axon.com>
2023-11-07 13:32:38 -0800

    Refactor find-pattern example to use official FindPattern middleware

--- _examples/find-pattern/main.go
+++ _examples/find-pattern/main.go
@@ -5,31 +5,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(FindPatternMiddleware(r))
+	r.Use(middleware.FindPattern(r, func(pattern string) {
+		fmt.Printf("pattern=%s\n", pattern)
+	}))
 
 	r.Get("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("hello, %s", chi.URLParam(r, "name"))))
 	})
 
 	http.ListenAndServe(":3333", r)
-}
-
-// Middleware that prints the route pattern before the request is handled
-func FindPatternMiddleware(router chi.Router) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			rctx := chi.NewRouteContext()
-			path := r.URL.Path
-			op := r.Method
-			pattern := router.Find(rctx, op, path)
-
-			fmt.Printf("pattern=%s\n", pattern)
-
-			next.ServeHTTP(w, r)
-		})
-	}
 }
