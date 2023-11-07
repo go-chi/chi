5cf9bc354372b1222ce690e7638e3bff464cd4cf
jriddle <jriddle@axon.com>
2023-11-07 13:10:37 -0800

    Rename find-pattern example

--- _examples/find-pattern/main.go
+++ _examples/find-pattern/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(ApiMiddleware(r))
+	r.Use(FindPatternMiddleware(r))
 
 	r.Get("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("hello, %s", chi.URLParam(r, "name"))))
@@ -18,16 +18,16 @@ func main() {
 	http.ListenAndServe(":3333", r)
 }
 
-// Middleware that prints the API pattern before the request is handled
-func ApiMiddleware(router chi.Router) func(http.Handler) http.Handler {
+// Middleware that prints the route pattern before the request is handled
+func FindPatternMiddleware(router chi.Router) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			rctx := chi.NewRouteContext()
 			path := r.URL.Path
 			op := r.Method
-			api := router.Find(rctx, op, path)
+			pattern := router.Find(rctx, op, path)
 
-			fmt.Printf("api=%s\n", api)
+			fmt.Printf("pattern=%s\n", pattern)
 
 			next.ServeHTTP(w, r)
 		})
