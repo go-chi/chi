bec368abbd9096918ef56ba88f26f6b545baf2ae
Gromet Spaal <gromms@users.noreply.github.com>
2024-01-23 02:21:48 +0200

    Fix a typo in maybe middleware doc (#889)

--- middleware/maybe.go
+++ middleware/maybe.go
@@ -4,7 +4,7 @@ import "net/http"
 
 // Maybe middleware will allow you to change the flow of the middleware stack execution depending on return
 // value of maybeFn(request). This is useful for example if you'd like to skip a middleware handler if
-// a request does not satisfied the maybeFn logic.
+// a request does not satisfy the maybeFn logic.
 func Maybe(mw func(http.Handler) http.Handler, maybeFn func(r *http.Request) bool) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
