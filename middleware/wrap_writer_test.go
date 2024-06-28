67be7d9cafdaeb4e04e887ff78d09e030ee43b00
Patryk Kalinowski <patryk@kalinowski.dev>
2024-06-28 16:29:27 +0200

    middleware: add Discard method to WrapResponseWriter (#926)
    * middleware: add Discard method to WrapResponseWriter
    
    * resolve review comments
    
    * use ioutil.Discard and deprecate the public interface
    
    * move the Discard method back to the public interface
    
    * discard calls to WriteHeader too

--- middleware/wrap_writer_test.go
+++ middleware/wrap_writer_test.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -21,4 +23,64 @@ func TestHttp2FancyWriterRemembersWroteHeaderWhenFlushed(t *testing.T) {
 	if !f.wroteHeader {
 		t.Fatal("want Flush to have set wroteHeader=true")
 	}
+}
+
+func TestBasicWritesTeesWritesWithoutDiscard(t *testing.T) {
+	// explicitly create the struct instead of NewRecorder to control the value of Code
+	original := &httptest.ResponseRecorder{
+		HeaderMap: make(http.Header),
+		Body:      new(bytes.Buffer),
+	}
+	wrap := &basicWriter{ResponseWriter: original}
+
+	var buf bytes.Buffer
+	wrap.Tee(&buf)
+
+	_, err := wrap.Write([]byte("hello world"))
+	assertNoError(t, err)
+
+	assertEqual(t, 200, original.Code)
+	assertEqual(t, []byte("hello world"), original.Body.Bytes())
+	assertEqual(t, []byte("hello world"), buf.Bytes())
+	assertEqual(t, 11, wrap.BytesWritten())
+}
+
+func TestBasicWriterDiscardsWritesToOriginalResponseWriter(t *testing.T) {
+	t.Run("With Tee", func(t *testing.T) {
+		// explicitly create the struct instead of NewRecorder to control the value of Code
+		original := &httptest.ResponseRecorder{
+			HeaderMap: make(http.Header),
+			Body:      new(bytes.Buffer),
+		}
+		wrap := &basicWriter{ResponseWriter: original}
+
+		var buf bytes.Buffer
+		wrap.Tee(&buf)
+		wrap.Discard()
+
+		_, err := wrap.Write([]byte("hello world"))
+		assertNoError(t, err)
+
+		assertEqual(t, 0, original.Code) // wrapper shouldn't call WriteHeader implicitly
+		assertEqual(t, 0, original.Body.Len())
+		assertEqual(t, []byte("hello world"), buf.Bytes())
+		assertEqual(t, 11, wrap.BytesWritten())
+	})
+
+	t.Run("Without Tee", func(t *testing.T) {
+		// explicitly create the struct instead of NewRecorder to control the value of Code
+		original := &httptest.ResponseRecorder{
+			HeaderMap: make(http.Header),
+			Body:      new(bytes.Buffer),
+		}
+		wrap := &basicWriter{ResponseWriter: original}
+		wrap.Discard()
+
+		_, err := wrap.Write([]byte("hello world"))
+		assertNoError(t, err)
+
+		assertEqual(t, 0, original.Code) // wrapper shouldn't call WriteHeader implicitly
+		assertEqual(t, 0, original.Body.Len())
+		assertEqual(t, 11, wrap.BytesWritten())
+	})
 }
