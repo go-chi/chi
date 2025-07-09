go-chi/chi

commit 0fe6bf1ba3ac601700b7993bc4c62f6c5f707932

Use RoutePath in URLFormat middleware (#718)

middleware/url_format.go
@@ -51,6 +51,11 @@ func URLFormat(next http.Handler) http.Handler {
 		var format string
 		path := r.URL.Path
 
+		rctx := chi.RouteContext(r.Context())
+		if rctx != nil && rctx.RoutePath != "" {
+			path = rctx.RoutePath
+		}
+
 		if strings.Index(path, ".") > 0 {
 			base := strings.LastIndex(path, "/")
 			idx := strings.LastIndex(path[base:], ".")
@@ -59,7 +64,6 @@ func URLFormat(next http.Handler) http.Handler {
 				idx += base
 				format = path[idx+1:]
 
-				rctx := chi.RouteContext(r.Context())
 				rctx.RoutePath = path[:idx]
 			}
 		}

middleware/url_format_test.go
@@ -48,3 +48,22 @@ func TestURLFormat(t *testing.T) {
 		t.Fatalf(resp)
 	}
 }
+
+func TestURLFormatInSubRouter(t *testing.T) {
+	r := chi.NewRouter()
+
+	r.Route("/articles/{articleID}", func(r chi.Router) {
+		r.Use(URLFormat)
+		r.Get("/subroute", func(w http.ResponseWriter, r *http.Request) {
+			articleID := chi.URLParam(r, "articleID")
+			w.Write([]byte(articleID))
+		})
+	})
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	if _, resp := testRequest(t, ts, "GET", "/articles/1/subroute.json", nil); resp != "1" {
+		t.Fatalf(resp)
+	}
+}

commit 081febb7e70a1f6870e0a2cd4e5131c7674f9df7

Avoid potential nil dereference
Fixes: 0fe6bf1 ("Use RoutePath in URLFormat middleware (#718)")
Found by PostgresPro with Svace Static Analyzer
Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

middleware/url_format.go
@@ -55,7 +55,7 @@ func URLFormat(next http.Handler) http.Handler {
 			path = rctx.RoutePath
 		}
 
-		if strings.Index(path, ".") > 0 {
+		if rctx != nil && strings.Index(path, ".") > 0 {
 			base := strings.LastIndex(path, "/")
 			idx := strings.LastIndex(path[base:], ".")
 