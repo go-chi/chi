988cc0f8cc17ed99864a1d2f0a8f8cb779417867
Arif Dogan <me@arif.sh>
2026-02-14 18:30:51 +0100

    Fix defaultLogEntry.Panic not respecting NoColor setting
    Panic() called PrintPrettyStack() which hardcodes useColor=true.
    Thread the useColor flag from defaultLogEntry through to
    prettyStack.parse() so it respects DefaultLogFormatter.NoColor.
    
    Fixes #1042

--- middleware/recoverer.go
+++ middleware/recoverer.go
@@ -52,9 +52,13 @@ func Recoverer(next http.Handler) http.Handler {
 var recovererErrorWriter io.Writer = os.Stderr
 
 func PrintPrettyStack(rvr interface{}) {
+	printPrettyStack(rvr, true)
+}
+
+func printPrettyStack(rvr interface{}, useColor bool) {
 	debugStack := debug.Stack()
 	s := prettyStack{}
-	out, err := s.parse(debugStack, rvr)
+	out, err := s.parse(debugStack, rvr, useColor)
 	if err == nil {
 		recovererErrorWriter.Write(out)
 	} else {
@@ -66,9 +70,8 @@ func PrintPrettyStack(rvr interface{}) {
 type prettyStack struct {
 }
 
-func (s prettyStack) parse(debugStack []byte, rvr interface{}) ([]byte, error) {
+func (s prettyStack) parse(debugStack []byte, rvr interface{}, useColor bool) ([]byte, error) {
 	var err error
-	useColor := true
 	buf := &bytes.Buffer{}
 
 	cW(buf, false, bRed, "\n")
